572c8cbd436a0bcc95d30c05a3bdae226ac47f99
Clément <duvivier.clem@gmail.com>
2022-09-23 14:04:30 +0200

    :bookmark: every components routes in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <Tutorial/>
+  <main>
+    <TheHeader/>
+    <TheFaqSection/>
+    <TheFooter/>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
 }
 </script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,600&display=swap');
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    color: $black;
+    font-family: "Rubik";
+    font-size: 13px;
+  }
+  main {
+    width: 100%;
+    height: 100%;
+
+  }
+</style>
