d07a33788196102e9c5dd5e89ea3c1f5180a9f3d
Clément <duvivier.clem@gmail.com>
2022-09-23 15:18:01 +0200

    :twisted_rightwards_arrows: merge front-dev

---
+++ components/TheFaqSection.vue
@@ -0,0 +1,161 @@
+<template>
+    <section class="faqSection">
+        <div class="standAubrac">
+            <div class="standAubracContainer">
+                <p>
+                Notre viande de boeuf vient directement de notre élevage en <span>Aubrac</span> 
+                ou le bien être animal est notre priorité.
+                Elle est garantie filière Bleu Blan Coeur. 
+                Nos animaux sont nourris au lin et au foin sans OGM, 
+                ni antibiotiques préventifs.
+                </p>
+                <img src="~/assets/bleu_blanc_coeur.png" alt="Bleu Blanc Coeur logo">
+            </div>
+        </div>
+        <div class="faqQuestions">
+            <h2>faq</h2>
+            <div v-for="question in questions" :key="question">
+                <details>
+                    <summary>{{question.question}}</summary>
+                    <p>{{question.answer}}</p>
+                </details>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Faq',
+        data() {
+            return {
+                questions: [
+                    {
+                        question: 'Comment est votre aligot ?',
+                        answer: 'Il est bon.',
+                    },
+                    {
+                        question: 'Votre viande est-elle d’origine française ?',
+                        answer: 'Notre viande est complètement d\'origine française, elle vient même de l\'Aubrac (ah c\'est marrant)',
+                    },
+                    {
+                        question: 'Peut-on privatiser le corner ?',
+                        answer: 'Carrément ! que ce soit pour n\'importe quelle raison ! (même pour fêter un mariage)',
+                    },
+                    {
+                        question: 'Organisez-vous des évènements ?',
+                        answer: 'lorem ipsum',
+                    }
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .faqSection {
+        margin: 0 auto;
+        height: 400px;
+        display: flex;
+        flex-direction: row;
+        gap: 50px;
+        .standAubrac {
+            width: 80%;
+            height: 100%;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-image: url("~/assets/stand_aubrac.jpg");
+            .standAubracContainer {
+                width: 100%;
+                height: 100%;
+                display: flex;
+                flex-direction: row;
+                justify-content: space-around;
+                align-items: center;
+                backdrop-filter: contrast(55%);
+                p {
+                    width: 50%;
+                    font-size: 16px;
+                    text-align: justify;
+                    color: $white;
+                    span {
+                        font-size: 16px;
+                        color: $primary;
+                    }
+                }
+
+                img {
+                    width: 65px;
+                    height: 65px;
+                }
+            }
+        }
+        .faqQuestions {
+            width: 80%;
+            margin: 0 auto;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+
+            h2 {
+                font-size: 40px;
+                text-transform: uppercase;
+                margin-bottom: 25px;
+            }
+
+            details {
+                transition: all 0.3s;
+                padding: 1em;
+                border-radius: 10px;
+                margin-bottom: 10px;
+                width: 100%;
+                box-shadow: 0px 2px 6px rgba(51, 50, 48, 0.15);
+                summary {
+                    display: flex;
+                    flex-direction: row-reverse;
+                    justify-content: space-between;
+                }
+            }
+
+            details > summary::before {
+                content: '▶️';
+            }
+
+            details[open] > summary::before {
+                content: '🔽';
+            }
+            details[open] {
+                padding: unset;
+                summary {
+                    padding: 0.5em;
+                    background-color: $black;
+                    color: $white;
+                    border-radius: 10px 10px 0 0;
+                }
+                p {
+                    padding: 0.5em;
+                }
+            }
+        }
+    }
+    @media screen and (max-width: 800px) {
+        .faqSection {
+            justify-content: center;
+            flex-direction: column-reverse;
+            height: fit-content;
+            .standAubrac {
+                margin-top: 20px;
+                margin: 0 auto;
+                height: 200px;
+                .standAubracContainer {
+                    p {
+                        font-size: 12px;
+                        span {
+                            font-size: 12px;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <header>
+        <nav>
+            <img class="NavLogo" src="~/assets/aubrac_logo.png" alt="logo aubrac">
+            <ul>
+                <li><a href="">le concept</a></li>
+                <li><a href="">nos produits</a></li>
+                <li><a href="">FAQ</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    nav {
+        .NavLogo {
+            width: 125px;
+            height: 35px;
+        }
+
+        background-color: $black;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        height: 70px;
+
+        ul {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: 50px;
+            a {
+                color: $white;
+            }
+        }
+    }
+    @media screen and (max-width: 700px) {
+        nav {
+        ul {
+            display: none;
+        }
+    }
+    }
+</style>
